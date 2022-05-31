f2a77d86c45edc592b874507b1e6df13b2323c5b
DenizShayan <92824050+DenizShayan@users.noreply.github.com>
2022-05-31 12:44:10 +0430

    3

--- script.js
+++ script.js
@@ -21,5 +21,27 @@ function increment(){
 // 1. create a function, save(), which logs out the count when it's called
 
 function save(){
+
+}
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+// Render the welcome message using welcomeEl.innerText
+
+let welcomeEl = document.getElementById(welcome-el);
+
+let myName="Deniz"
+
+function yourName(name){
+    return name;
+}
+
+function greeting(name){
+    // return "Welcome dear "+ name;
+
     
+
 }
